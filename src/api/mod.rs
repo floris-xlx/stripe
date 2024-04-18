3f4665e391231ebda3e240317b64d80385abf9d3
Floris <floris@xylex.ai>
2024-04-18 09:51:40 +0200

    Update mod.rs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,25 @@
+//! ## API endpoints to expose for Stripe
+//!
+//! ### Table of contents
+
+
 pub mod client;
 pub mod errors;
 pub mod events;
-pub mod success;  
+pub mod success;
+
+
+/// ## Base construction for the `Api`
+///
+/// ### Fields
+/// - `host` - This is the host port that the api is exposed under
+/// - `port` - This is the port that the the api is exposed under
+///
+pub struct Api {
+    pub host: String,
+    pub port: i16
+}
+
+
+
+
